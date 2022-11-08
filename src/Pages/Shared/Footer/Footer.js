b4a737792f1215a842bd220cf877415b8ec7dcaa
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-11-08 09:56:18 +0600

    created websites footer with some copyright text only

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+      <div>
+        <p>Copyright © 2022 - All right reserved by Cook and Taste</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
