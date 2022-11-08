44c0db32ff4469dfd76e4017e7eca67e40d0637b
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-11-08 10:13:21 +0600

    created home route and some routes and created pages with some dummy text
    changed favicon

--- src/routes/routes/routes.js
+++ src/routes/routes/routes.js
@@ -1,18 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
+import ErrorPage from "../../Pages/ErrorPage/ErrorPage";
+import Home from "../../Pages/Home/Home";
+import Login from "../../Pages/Login/Login";
+import Register from "../../Pages/Register/Register";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
-        element: <></>,
+        element: <Home />,
       },
       {
         path: "home",
-        element: <></>,
+        element: <Home />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
       },
     ],
   },
