d5ae70f408c4c2a347bf5f477e1e9cd4e70995d0
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-11-10 13:57:16 +0600

    created an error page and order confirm button

--- src/Pages/ErrorPage/ErrorPage.js
+++ src/Pages/ErrorPage/ErrorPage.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import useChangeTitle from "../../hooks/useChangeTitle";
 
 const ErrorPage = () => {
   useChangeTitle("Page not found");
   return (
-    <div>
-      <h1>Hello from error page</h1>
+    <div className="min-h-screen p-6 md:p-10 lg:p-16 flex justify-center items-center">
+      <div>
+        <h3 className="text-4xl text-center text-red-500 mb-5">
+          Something went wrong
+        </h3>
+        <div className="flex justify-center">
+          <Link to="/">
+            <button className="btn btn-warning">Go back Home</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/RecipeDetailsHeader/RecipeDetailsHeader.js
+++ src/components/RecipeDetailsHeader/RecipeDetailsHeader.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const RecipeDetailsHeader = ({ imgUrl }) => {
   const headerStyle = {
@@ -9,10 +11,28 @@ const RecipeDetailsHeader = ({ imgUrl }) => {
     bakcgroundPosition: "center center",
   };
 
+  const orderConfirmHandler = () => {
+    toast.success("Your food will be on your doorstep soon. Enjoy the food.", {
+      position: "top-center",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  };
+
   return (
     <header className="hero mb-6" style={headerStyle}>
       <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-center text-neutral-content"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <button onClick={orderConfirmHandler} className="btn btn-primary">
+          Place an Order
+        </button>
+      </div>
+      <ToastContainer />
     </header>
   );
 };
